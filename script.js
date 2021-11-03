f1829768ef862a83d23b2c459d877e6c79d1a049
Pierre <pierre.lagadec@gmail.com>
2021-11-03 12:09:37 +1100

    fine tunes scrolling loading behaviour

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photos_array = [];
 
 //unsplash API
 //count - The number of photos to return. (Default: 1; max: 30)
 //topics - Public topic ID(‘s) to filter selection. If multiple, comma-separated
-const count = 10;
+const count = 30;
 const apiKey = 'iO7SEah7t2HgBtjQePvtHrfmVM9lMOC8SiOmem1Cx8k';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//image loaded listener
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        imagesLoaded = 0;        
+        loader.hidden = true;
+    }
+}
 
 //setAttribute helper function
 function setAttributes(element, attributes) {
@@ -19,7 +32,7 @@ function setAttributes(element, attributes) {
 
 //create elements for links and photos
 function displayPhotos() {
-    
+    totalImages = photos_array.length;
     photos_array.forEach((photo) => {
         //create <a> to link to source (unsplash)
         const link = document.createElement('a');
@@ -34,6 +47,8 @@ function displayPhotos() {
             alt: photo.description,
             title: photo.description
         })
+        //event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         //put <img> inside <a>, then both in imageContainer
         link.appendChild(img);
         imageContainer.appendChild(link);
@@ -45,7 +60,6 @@ async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photos_array = await response.json();
-        console.log(photos_array);
         displayPhotos();
         
     } catch(error) {
@@ -59,8 +73,9 @@ window.addEventListener('scroll', (event) => {
     //window.scrollY - position of vertical scroll
     //body.offsetHeight - total height of document
     if(window.innerHeight + window.scrollY 
-        >= document.body.offsetHeight - 1000) {
-        console.log();
+        >= document.body.offsetHeight - 800
+        && ready) {
+        ready = false;
         getPhotos();
     }
 });
